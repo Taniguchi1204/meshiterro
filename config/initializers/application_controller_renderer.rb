f33c883b2b2211065c8d3d25109a997535a439e9
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-06-26 11:19:36 +0000

    create postcomment table

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2be7d0ba4565d5c8f5fc4e69387e7b48fb3cef1e1ab5aa2a885a8bfa938405395e529ffc3036d0f25dd30527e89f309ce7bd24a3ea9d5dcb3335f66298b51afa'
